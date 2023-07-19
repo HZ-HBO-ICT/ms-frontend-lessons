ca1d123cc9087d2ed8aa84a81a9fee3cae053f40
Rimmert Zelle <rimmertzelle@192.168.68.107>
2023-07-19 09:39:44 +0200

    added step 4 frontend talking to microservice

---
+++ step-4-api/src/lib/components/Client.svelte
@@ -0,0 +1,5 @@
+<script>
+    let client;
+</script>
+
+<li bind:this={clientDetails} on:click={displayClientDetails} class="inline-block p-2 hover:bg-violet-600 cursor-pointer">{client.appointment.name}</li>
